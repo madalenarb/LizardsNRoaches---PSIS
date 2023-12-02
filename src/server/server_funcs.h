7fcbcd547fe4e052034b61ba93dbda05fb80749f
Joaobarros10 <144815467+Joaobarros10@users.noreply.github.com>
2023-12-02 00:59:57 +0000

    Update server_funcs.h

--- src/server/server_funcs.h
+++ src/server/server_funcs.h
@@ -10,6 +10,16 @@
 #include <stdlib.h>
 #include <zmq.h>
 
+typedef enum direction_t {UP, DOWN, LEFT, RIGHT} direction_t;
+
+typedef struct remote_char_t
+{   
+    int msg_type; /* 0 join   1-move */
+    char ch; 
+    direction_t direction ;
+    /* data */
+}remote_char_t;
+
 typedef struct ch_info_t
 {
     int ch;
