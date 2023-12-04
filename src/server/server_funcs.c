be15b7382e7a71a3e7d91e476396d2fd4f05ade1
Joaobarros10 <144815467+Joaobarros10@users.noreply.github.com>
2023-12-04 17:13:46 +0000

    Update server_funcs.c

--- src/server/server_funcs.c
+++ src/server/server_funcs.c
@@ -1,6 +1,6 @@
 #include "server_funcs.h"
 
-void new_position(int* x, int *y, int *cauda_x[], int *cauda_y[], direction_t direction){
+void new_position(int* x, int *y, int cauda_x[], int cauda_y[], direction_t direction){
         int i;
         switch (direction)
         {
@@ -8,27 +8,29 @@ void new_position(int* x, int *y, int *cauda_x[], int *cauda_y[], direction_t di
             (*x) --;
             for (i=0;i<5;i++) //a cauda fica na mesma direção da cabeça
             {
-                *cauda_y[i]=*y;
+                cauda_y[i]=*y;
             }
             if(*x ==0){ //caso va contra a parede
                 
                 for(i=0;i<5;i++)
-                    *cauda_x[i]=i+2;
+                {
+                    cauda_x[i]=i+2;
+                }
                 *x = 7; //5 da cauda + 2
             }
             else if(*x==WINDOW_SIZE-2 || *x==WINDOW_SIZE-3 || *x==WINDOW_SIZE-4 || *x==WINDOW_SIZE-5) //para a cauda ficar dentro da janela
             {
                 *x = WINDOW_SIZE-7;
                 for (i=0;i<5;i++)
-                    {
-                        *cauda_x[i]=WINDOW_SIZE-2-i;
-                    }
+                {
+                    cauda_x[i]=WINDOW_SIZE-2-i;
+                }
             }
             else
             { 
                 for (i=0;i<5;i++)
                 {
-                    *cauda_x[i]= (*x)+i+1;
+                    cauda_x[i]= (*x)+i+1;
                 }
             }
             break;
@@ -37,14 +39,14 @@ void new_position(int* x, int *y, int *cauda_x[], int *cauda_y[], direction_t di
             (*x) ++;
             for (i=0;i<5;i++) //a cauda fica na mesma direção da cabeça
             {
-                *cauda_y[i]=*y;
+                cauda_y[i]=*y;
             }
 
             if(*x ==WINDOW_SIZE-1) //caso va contra a parede
             {
                 for(i=0;i<5;i++)
                 {
-                    *cauda_x[i]=WINDOW_SIZE-i-3;
+                    cauda_x[i]=WINDOW_SIZE-i-3;
                 }
                 *x = WINDOW_SIZE-8;
             }
@@ -52,15 +54,15 @@ void new_position(int* x, int *y, int *cauda_x[], int *cauda_y[], direction_t di
             {
                 *x = 6;
                 for (i=0;i<5;i++)
-                    {
-                        *cauda_x[i]=i+1;
-                    }
+                {
+                    cauda_x[i]=i+1;
+                }
             }
             else
             {
                 for (i=0;i<5;i++)
                 {
-                    *cauda_x[i]= (*x)-i-1;
+                    cauda_x[i]= (*x)-i-1;
                 }
             }
             break;
@@ -68,27 +70,29 @@ void new_position(int* x, int *y, int *cauda_x[], int *cauda_y[], direction_t di
             (*y) --;
             for (i=0;i<5;i++) //a cauda fica na mesma direção da cabeça
             {
-                *cauda_x[i]=*x;
+                cauda_x[i]=*x;
             }
-            if(*y ==0){ //caso va contra a parede
-                
+            if(*y ==0) //caso va contra a parede
+            {
                 for(i=0;i<5;i++)
-                    *cauda_y[i]=i+2;
+                {
+                    cauda_y[i]=i+2;
+                }
                 *y = 7; //5 da cauda + 2
             }
             else if(*y==WINDOW_SIZE-2 || *y==WINDOW_SIZE-3 || *y==WINDOW_SIZE-4 || *y==WINDOW_SIZE-5) //para a cauda ficar dentro da janela
             {
                 *y = WINDOW_SIZE-7;
                 for (i=0;i<5;i++)
-                    {
-                        *cauda_y[i]=WINDOW_SIZE-2-i;
-                    }
+                {
+                    cauda_y[i]=WINDOW_SIZE-2-i;
+                }
             }
             else
             { 
                 for (i=0;i<5;i++)
                 {
-                    *cauda_y[i]= (*x)+i+1;
+                    cauda_y[i]= (*x)+i+1;
                 }
             }
             break;
@@ -97,14 +101,14 @@ void new_position(int* x, int *y, int *cauda_x[], int *cauda_y[], direction_t di
             (*y) ++;
             for (i=0;i<5;i++) //a cauda fica na mesma direção da cabeça
             {
-                *cauda_x[i]=*x;
+                cauda_x[i]=*x;
             }
 
             if(*y ==WINDOW_SIZE-1) //caso va contra a parede
             {
                 for(i=0;i<5;i++)
                 {
-                    *cauda_y[i]=WINDOW_SIZE-i-3;
+                    cauda_y[i]=WINDOW_SIZE-i-3;
                 }
                 *y = WINDOW_SIZE-8;
             }
@@ -112,15 +116,15 @@ void new_position(int* x, int *y, int *cauda_x[], int *cauda_y[], direction_t di
             {
                 *y = 6;
                 for (i=0;i<5;i++)
-                    {
-                        *cauda_y[i]=i+1;
-                    }
+                {
+                    cauda_y[i]=i+1;
+                }
             }
             else
             {
                 for (i=0;i<5;i++)
                 {
-                    *cauda_y[i]= (*x)-i-1;
+                    cauda_y[i]= (*x)-i-1;
                 }
             }
             break;
