478872d0a2329adabc6d44170dac64f2f73e71e2
Joaobarros10 <144815467+Joaobarros10@users.noreply.github.com>
2023-12-05 15:17:27 +0000

    Update server_funcs.c
    1 erro corrigido

--- src/server/server_funcs.c
+++ src/server/server_funcs.c
@@ -18,7 +18,7 @@ void new_position(int* x, int *y, int cauda_x[], int cauda_y[], direction_t dire
                 }
                 *x = 7; //5 da cauda + 2
             }
-            else if(*x==WINDOW_SIZE-2 || *x==WINDOW_SIZE-3 || *x==WINDOW_SIZE-4 || *x==WINDOW_SIZE-5) //para a cauda ficar dentro da janela
+            else if(*x==WINDOW_SIZE-2 || *x==WINDOW_SIZE-3 || *x==WINDOW_SIZE-4 || *x==WINDOW_SIZE-5|| *x==WINDOW_SIZE-6) //para a cauda ficar dentro da janela
             {
                 *x = WINDOW_SIZE-7;
                 for (i=0;i<5;i++)
@@ -50,7 +50,7 @@ void new_position(int* x, int *y, int cauda_x[], int cauda_y[], direction_t dire
                 }
                 *x = WINDOW_SIZE-8;
             }
-            else if (*x==1 || *x==2 || *x==3 || *x==4) //para a cauda ficar dentro da janela
+            else if (*x==1 || *x==2 || *x==3 || *x==4 || *x==5) //para a cauda ficar dentro da janela
             {
                 *x = 6;
                 for (i=0;i<5;i++)
@@ -80,7 +80,7 @@ void new_position(int* x, int *y, int cauda_x[], int cauda_y[], direction_t dire
                 }
                 *y = 7; //5 da cauda + 2
             }
-            else if(*y==WINDOW_SIZE-2 || *y==WINDOW_SIZE-3 || *y==WINDOW_SIZE-4 || *y==WINDOW_SIZE-5) //para a cauda ficar dentro da janela
+            else if(*y==WINDOW_SIZE-2 || *y==WINDOW_SIZE-3 || *y==WINDOW_SIZE-4 || *y==WINDOW_SIZE-5 || *y==WINDOW_SIZE-6 ) //para a cauda ficar dentro da janela
             {
                 *y = WINDOW_SIZE-7;
                 for (i=0;i<5;i++)
@@ -112,7 +112,7 @@ void new_position(int* x, int *y, int cauda_x[], int cauda_y[], direction_t dire
                 }
                 *y = WINDOW_SIZE-8;
             }
-            else if (*y==1 || *y==2 || *y==3 || *y==4) //para a cauda ficar dentro da janela
+            else if (*y==1 || *y==2 || *y==3 || *y==4 || *y==5) //para a cauda ficar dentro da janela
             {
                 *y = 6;
                 for (i=0;i<5;i++)
