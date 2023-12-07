224d5b9b4f785e529e21ca072f9470c7cd453aa5
Joaobarros10 <144815467+Joaobarros10@users.noreply.github.com>
2023-12-07 21:38:39 +0000

    Update protocol.h

--- common/protocol.h
+++ common/protocol.h
@@ -36,6 +36,7 @@ typedef struct message_t
     char password[20];
     int N_roaches; //
     int score_roaches[10];
+    int i; //para saber as coordenadas
     char id; // id given by the server, the client uses it to verify
     direction_t direction;
 } message_t;
