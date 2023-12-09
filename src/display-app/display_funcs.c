ea90dcb501807f30f9a18f10dce68a8bb510657b
Joaobarros10 <144815467+Joaobarros10@users.noreply.github.com>
2023-12-09 20:09:07 +0000

    Update display_funcs.c

--- src/display-app/display_funcs.c
+++ src/display-app/display_funcs.c
@@ -1,1 +1,14 @@
 // This is Lizard-funcs.c
+// This is Lizard-funcs.c
+// display_funcs.c
+#include "display_funcs.h"
+
+void setupWindows(WINDOW **my_win){
+    initscr();			/* Start curses mode 		*/
+    cbreak();
+    keypad(stdscr, TRUE);
+    noecho();
+    *my_win = newwin(WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0);
+    box(*my_win, 0 , 0);
+    wrefresh(*my_win);
+}
