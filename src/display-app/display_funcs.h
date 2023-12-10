703271800b4f35470a80b34c435bdff7059fc245
Joaobarros10 <144815467+Joaobarros10@users.noreply.github.com>
2023-12-10 02:43:22 +0000

    Update display_funcs.h

--- src/display-app/display_funcs.h
+++ src/display-app/display_funcs.h
@@ -1,21 +1,16 @@
-
-// This is Lizard-funcs.c
 // display_funcs.h
 #ifndef DISPLAY_FUNCS_H
 #define DISPLAY_FUNCS_H
 
+#include "../../common/constants.h" //nao tava a ir buscar
+#include "../../common/protocol.h"
 #include <ncurses.h>
 #include <assert.h>
 
-//#include "../../common/constants.h" //nao tava a ir buscar
-//#include "../../common/protocol.h"
+
 
 #define WINDOW_WIDTH 30
 #define WINDOW_HEIGHT 30
-
-typedef struct {
-    int content[30][30];
-} display_message_t;
 
 void setupWindows(WINDOW **my_win);
 
